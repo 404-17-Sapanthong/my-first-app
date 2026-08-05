075dd99bc580c1a4a53af02b4e77bc13640b62da
404-17-Sapanthong <54427@yupparaj.ac.th>
2026-08-05 16:15:38 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,4 +1,4 @@
- import streamlit as st
+import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
@@ -7,4 +7,3 @@ st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นาย สพานทอง ภูสิริพัฒนานนท์ เลขที่ 17  ม.4/4")
-
